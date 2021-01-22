794b78a0e9d9c2380c3df325d6a6e96496ea799e
Saquib <saquibjawed0@gmail.com>
2021-01-22 18:37:45 +0530

    initial commit

--- contact/settings.py
+++ contact/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,8 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'contact/static')]
-# STATIC_ROOT=os.path.join(BASE_DIR,'asset')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+
+
+django_heroku.settings(locals())
+
